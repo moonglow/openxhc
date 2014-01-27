a9384b9ca94ca9be9524c893d32f7cd217b7c3c4
fxd <fxd>
2014-01-27 20:53:17 +0200

    base for new HD44780 displays driver

---
+++ firmware/src/hd44780.c
@@ -0,0 +1,225 @@
+/*********************************************************************\
+* Copyleft (>) 2014 Roman 'Fallout' Ilichev <fxdteam@gmail.com>      *
+ *                                                                   *
+ * This file is part of OpenXHC project                              *
+ *                             WTFPL LICENSE v2                      *
+\*********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stm32f10x.h"
+#include "io_macro.h"
+#include "xhc_dev.h"
+
+#include "string_utils.h"
+
+#include "lcd_driver.h"
+/* 
+  20x4 & 16x2
+  DRIVER: HD44780
+  MODE: 4bit, R/W always 0
+*/
+
+/* 
+  HW connection: 
+  PA2 - E
+  PA3 - RS
+  GND - R/W
+  GND - D0, D1, D2, D3
+  PA4 - D4
+  PA5 - D5
+  PA6 - D6
+  PA7 - D7
+*/
+
+
+#define LCD_E           A, 2, SPEED_50MHz
+#define LCD_RS          A, 3, SPEED_50MHz
+
+#define LCD_D4          A, 4, SPEED_50MHz
+#define LCD_D5          A, 5, SPEED_50MHz
+#define LCD_D6          A, 6, SPEED_50MHz
+#define LCD_D7          A, 7, SPEED_50MHz
+
+/* delay timer */
+#define TIMER_DEV       TIM3
+#define TIMER_APB       RCC_APB1PeriphClockCmd
+#define TIMER_CLOCK     RCC_APB1Periph_TIM3
+
+
+/* used for uS delays */
+static void hw_timer_init( void )
+{
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+  
+  TIMER_APB( TIMER_CLOCK, ENABLE );  
+  
+  TIM_DeInit( TIMER_DEV );
+  
+  /*  xx000000/xx = 1MHz */
+  TIM_TimeBaseStructure.TIM_Prescaler = SystemCoreClock/1000000ul;
+  TIM_TimeBaseStructure.TIM_Period = 0; /* use ARR as delay */
+  TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+ 
+  TIM_TimeBaseInit( TIMER_DEV, &TIM_TimeBaseStructure);
+  
+  TIMER_DEV->CNT = 0;
+  /* timer will switch off automaticaly, after counter will equal to ARR */
+  TIM_SelectOnePulseMode( TIMER_DEV, TIM_OPMode_Single );
+}
+
+static void delay_us( uint16_t us )
+{
+    /* long wire compensation */
+    TIMER_DEV->ARR = (us*16);
+    TIMER_DEV->CR1 |= TIM_CR1_CEN;
+    while( TIMER_DEV->CR1 & TIM_CR1_CEN );
+}
+
+
+static void lcd_nibble_out( uint8_t n )
+{
+  if( n & 0x01 )
+    PIN_HI( LCD_D4 );
+  else
+    PIN_LOW( LCD_D4 );
+
+  if( n & 0x02)
+    PIN_HI( LCD_D5 );
+  else
+    PIN_LOW( LCD_D5 );
+      
+  if( n & 0x04)
+    PIN_HI( LCD_D6 );
+  else
+    PIN_LOW( LCD_D6 );
+
+  if( n & 0x08)
+    PIN_HI( LCD_D7 );
+  else
+    PIN_LOW( LCD_D7 );
+  
+  PIN_HI( LCD_E );
+  delay_us( 10 );
+  PIN_LOW( LCD_E );
+  delay_us( 50 );
+}
+
+static void lcd_write_byte( uint8_t d )
+{
+  lcd_nibble_out( d>>4 );
+  lcd_nibble_out( d );
+}
+
+static void lcd_write_cmd( uint8_t c )
+{
+  PIN_LOW( LCD_RS );
+  delay_us( 50 );
+  lcd_write_byte( c );
+}
+
+static void lcd_write_data( uint8_t d )
+{
+  PIN_HI( LCD_RS );
+  delay_us( 50 );
+  lcd_write_byte( d );
+}
+
+static void hd44780_hw_init( void )
+{
+  hw_timer_init();
+  
+  PORT_ENABLE_CLOCK_START()
+      PORT_ENABLE_CLOCK_ENTRY( LCD_E )
+      PORT_ENABLE_CLOCK_ENTRY( LCD_RS )
+      PORT_ENABLE_CLOCK_ENTRY( LCD_D4 )
+      PORT_ENABLE_CLOCK_ENTRY( LCD_D5 )
+      PORT_ENABLE_CLOCK_ENTRY( LCD_D6 )
+      PORT_ENABLE_CLOCK_ENTRY( LCD_D7 )
+  PORT_ENABLE_CLOCK_END()
+      
+  PIN_LOW( LCD_E );
+  PIN_LOW( LCD_RS );
+  
+  PIN_LOW( LCD_D4 );
+  PIN_LOW( LCD_D5 );
+  PIN_LOW( LCD_D6 );
+  PIN_LOW( LCD_D7 );
+     
+  PIN_OUT_PP( LCD_E );
+  PIN_OUT_PP( LCD_RS );
+  
+  PIN_OUT_PP( LCD_D4 );
+  PIN_OUT_PP( LCD_D5 );
+  PIN_OUT_PP( LCD_D6 );
+  PIN_OUT_PP( LCD_D7 );
+
+  delay_us( 1000 );
+  
+  /* 4 bit mode */
+  lcd_nibble_out( 0x03 );
+  delay_us( 1000 );
+  lcd_nibble_out( 0x03 );
+  delay_us( 1000 );
+  lcd_nibble_out( 0x03 );
+  delay_us( 1000 );
+  
+  /* 4 bit mode */
+  lcd_nibble_out( 0x02 );
+  delay_us( 50 );
+  
+  /* function set 5x8 */
+  lcd_write_cmd( 0x28 );
+  delay_us( 50 );
+  
+  /* display on/off control */
+  lcd_write_cmd( 0x0C );
+  delay_us( 50 );
+ 
+  /* entry mode */
+  lcd_write_cmd( 0x06 );
+  delay_us( 50 );
+  
+  /* clear screen */
+  lcd_write_cmd( 0x01 );
+  delay_us( 2000 );
+  
+  __IO char tmp[32];
+  xhc2string( 0, 1, 4, 4, (char*)tmp );
+  lcd_driver.draw_text( (char*)tmp, 0, 0 );
+  for(;; );
+}
+
+static void hd44780_clear(void)
+{
+    lcd_write_cmd( 0x01 );
+    /* about 1.53ms to execute */
+    delay_us( 2000 );
+}
+
+static const uint8_t row_addr_table[] = { 0x00, 0x40, 0x14, 0x54 };
+static void hd44780_write_string( char *s, int x, int y )
+{
+  lcd_write_cmd( (0x80 | row_addr_table[y]) + x ); 
+  delay_us( 50 );
+  while ( *s )
+  {
+    lcd_write_data( *s++ );
+  }
+}
+
+static void hd44780_render_screen( void *p, uint8_t mode, uint8_t mode_ex )
+{
+    
+}
+
+const struct t_lcd_driver lcd_driver = 
+{
+  .init = hd44780_hw_init,
+  .exit = 0,
+  .render_screen = hd44780_render_screen,
+  .clear_screen = hd44780_clear,
+  .draw_bmp = 0,
+  .draw_text = hd44780_write_string,
+};
